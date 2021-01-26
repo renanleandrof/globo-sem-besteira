b601fef0ab0fe515407c04308cc950d658e7ad51
Renan Leandro Ferreira <renanleandrof@gmail.com>
2021-01-25 21:22:49 -0300

    Versão atualizada 2021

--- contentscript.js
+++ contentscript.js
@@ -4,24 +4,6 @@
  * LICENSE file.
  */
 
- 
- $('.glb-hl-style-entretenimento').each(function(){ $(this).hide();});
- $('.etc-column').each(function(){ $(this).hide();});
- $('.entretenimento').each(function(){ $(this).hide();});
- $('.famosos-style').each(function(){ $(this).hide();});
- $('.tvg').each(function(){ $(this).hide();});
- 
- if ($("div#urgente").html().indexOf("bbb") != -1) {
-	$("div#urgente").hide();
- } 
- 
- 
-  var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-37763628-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+$('[data-tracking-action="entretenimento"]').each(function(){ 
+  $(this).html("<h3 class='highlight-font-title-column  homeui-tc-entretenimento'>Protegido pelo Globo Sem Besteirol</h3>");
+});
